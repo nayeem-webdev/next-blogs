cc2235afef58ba2dc147db7a80c3eb6c1789b171
Nayeem Uddin <unayeem33@gmail.com>
2025-01-24 17:15:01 +0600

    hello

--- src/app/profile/page.jsx
+++
@@ -1,5 +0,0 @@
-const Profile = () => {
-  return <div>hello world from Profile</div>;
-};
-
-export default Profile;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: "class",
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
